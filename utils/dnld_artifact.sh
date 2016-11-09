4028797c6d6f6ce29bf8a5447a257f5261aff23c
woorulez <woorulez@gmail.com>
2016-11-09 22:18:44 +0900

    Update dnld_artifact.sh

--- utils/dnld_artifact.sh
+++ utils/dnld_artifact.sh
@@ -15,7 +15,7 @@ filename="$artifactid-$version${classifier:+-$classifier}.jar"
 linkname="$artifactid${classifier:+-$classifier}.jar"
 
 echo "downloading $artifact to $filename..."
-(set -o pipefail; mvn org.apache.maven.plugins:maven-dependency-plugin:2.4:get -Dartifact=$artifact -Ddest=./$filename -DremoteRepositories="$repourl" 2>&1 | tee upgrade.log)
+(set -o pipefail; mvn org.apache.maven.plugins:maven-dependency-plugin:2.4:get -Dartifact=$artifact -Ddest=./$filename -DremoteRepositories="$repourl" -Dtransitive=false 2>&1 | tee upgrade.log)
 if [ $? -eq 0 ]; then
     echo "linking $linkname to $filename..."
     ln -sf $filename $linkname

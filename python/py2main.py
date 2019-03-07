6b39b2d11f62070294d607141638788bd30ed48a
woorulez <woorulez@gmail.com>
2019-03-07 13:41:48 +0900

    Create py2main.py

---
+++ python/py2main.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+import requests
+
+def main(argv = None):
+    print("Hello World!")
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())

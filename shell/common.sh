1ac98f31c7ad09eb24796381fa09b566dba4f67d
woo-choi <80515729+woo-choi@users.noreply.github.com>
2021-06-11 20:52:00 +0900

    Update common.sh

--- shell/common.sh
+++ shell/common.sh
@@ -1,4 +1,8 @@
 #! /usr/bin/env bash
 
+set -o errexit # set -e
+set -o pipefail
+set -o nounset # set -u
+
 SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
 echo "$SCRIPT_DIR"
